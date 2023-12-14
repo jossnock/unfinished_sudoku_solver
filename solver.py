fbc95edbdae625cdd0c142378d0f8f513dd0bd04
jossnock <19121867@hlnsc.ac.uk>
2023-12-14 08:54:49 +0000

    begin layout of steps

---
+++ solver.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+"""
+Steps:
+ - Iterate through each cell, checking its row, column, and box
+ - Assign each cell a list of possible values.
+ - Check for naked/hidden singles, naked/hidden doubles
+ - 
+ - 
+ - 
+ - 
+ - 
+ - 
+"""
