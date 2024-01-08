d8fdb4c0a1bf338a72c3bc831e51a97d094f0a5a
JossNockolds <19121867@hlnsc.ac.uk>
2024-01-08 18:54:37 +0000

    implemented board input

--- solver.py
+++ solver.py
@@ -1,10 +1,44 @@
+# -1 to go back
 
+def fill_board():
+    """Enter the board; if you make a mistake, enter -1 to go back.\n
+    Each input must be an integer between 1 and 9 inclusive.\n
+    Start by listing the digits to the left of the top row, then from left to right.
+    Repeat for the rest of the rows, in descending order.\n
+    For empty cells, put 0"""
+    print("Enter the board; if you make a mistake, enter -1 to go back.\n Each input must be an integer between 1 and 9 inclusive.\n Start by listing the digits to the left of the top row, then from left to right. Repeat for the rest of the rows, in descending order.\n For empty cells, put 0")
+    board = [[[] for i in range(9)] for i in range(9)]
+    i = 0
+    while i < 9:
+        j = 0
+        while j < 9:
+            digit = input("Row: {} Column: {}\n".format(i+1, j+1))
+            try:
+                int(digit)
+            except ValueError:
+                print("the number must be an integer, try again")
+            else: 
+                digit = int(digit)
+                if digit == -1:
+                    j -= 1
+                else:
+                    if ((digit >= 0) and (digit <=9)):
+                        if digit == 0:
+                            board[i][j] = (1,2,3,4,5,6,7,8,9)
+                        else:
+                            board[i][j] = digit
+                        j += 1
+                    else:
+                        print("the number must be between 1 and 9 inclusive, try again")
+        i += 1
+    return board
 
+def sudoku_print(board):
+    for i in range(9):
+        print(board[i])
 
-
-
-
-
+#sudoku_print(fill_board())
+#sudoku_print([[1]*9]*9)
 
 
 
