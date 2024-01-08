e0d3ad90eecd4919b84b11a38d1836f9f5189d20
JossNockolds <19121867@hlnsc.ac.uk>
2024-01-08 19:43:38 +0000

    formatting

--- solver.py
+++ solver.py
@@ -1,18 +1,28 @@
-# -1 to go back
+def sudoku_print(board):
+    for i in range(9):
+        for j in range(9):
+            #test if the cell value is a tuple (meaning it's digit isn't known yet)
+            if type(board[i][j]) == type(('','')):
+                print('?', end= ' ')
+            else:    
+                print(board[i][j], end= ' ')
+        print('')
 
 def fill_board():
-    """Enter the board; if you make a mistake, enter -1 to go back.\n
+    """Input the board; if you make a mistake, input -1 to go back.\n
     Each input must be an integer between 1 and 9 inclusive.\n
     Start by listing the digits to the left of the top row, then from left to right.
     Repeat for the rest of the rows, in descending order.\n
-    For empty cells, put 0"""
-    print("Enter the board; if you make a mistake, enter -1 to go back.\n Each input must be an integer between 1 and 9 inclusive.\n Start by listing the digits to the left of the top row, then from left to right. Repeat for the rest of the rows, in descending order.\n For empty cells, put 0")
+    For empty cells, input 0"""
+    print("Input the board; if you make a mistake, input -1 to go back.\n Each input must be an integer between 1 and 9 inclusive.\n Start by listing the digits to the left of the top row, then from left to right. Repeat for the rest of the rows, in descending order.\n For empty cells, input 0")
     board = [[[] for i in range(9)] for i in range(9)]
     i = 0
     while i < 9:
         j = 0
         while j < 9:
+            # lists the current row and column of the digit to be inputted (+1 so 9 is the max):
             digit = input("Row: {} Column: {}\n".format(i+1, j+1))
+            #checks if integer:
             try:
                 int(digit)
             except ValueError:
@@ -20,25 +30,25 @@ def fill_board():
             else: 
                 digit = int(digit)
                 if digit == -1:
-                    j -= 1
+                    j -= 1 # j becomes previous j so the user can re-try their input
                 else:
-                    if ((digit >= 0) and (digit <=9)):
-                        if digit == 0:
-                            board[i][j] = (1,2,3,4,5,6,7,8,9)
+                    if ((digit >= 0) and (digit <=9)): # if digit is 1-9 inclusive
+                        if digit == 0: # if cell is unknown, assigns it its pencil values:
+                            board[i][j] = (1,2,3,4,5,6,7,8,9) 
                         else:
                             board[i][j] = digit
                         j += 1
                     else:
                         print("the number must be between 1 and 9 inclusive, try again")
         i += 1
+    sudoku_print(board)
     return board
 
-def sudoku_print(board):
-    for i in range(9):
-        print(board[i])
+
+fill_board()
+#sudoku_print([[0]*9]*9)
 
 #sudoku_print(fill_board())
-#sudoku_print([[1]*9]*9)
 
 
 
